6f521d2dc4548605c398cbdf7358443ac63c1aaa
Bruno <bferronato@gmail.com>
2016-07-10 16:28:11 -0300

    Update IndexController.php

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -5,7 +5,7 @@ class IndexController extends Zend_Controller_Action
 
     public function init()
     {
-	$this->_helper->layout->setLayout ( 'login' );
+	$this->_helper->layout->setLayout('login');
     }
 
     public function indexAction()
