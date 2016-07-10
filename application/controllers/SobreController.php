c0fa2492e4f3a087bdef60bee2eea1da9d4d4721
Bruno Henrique Ferronato <bferronato@gmail.com>
2016-07-10 20:56:19 -0300

    Sobre

---
+++ application/controllers/SobreController.php
@@ -0,0 +1,15 @@
+<?php
+
+class SobreController extends Zend_Controller_Action
+{
+
+    public function init()
+    {
+        $this->_helper->layout->setLayout('sistema');
+    }
+
+    public function indexAction()
+    {
+    }
+
+}
