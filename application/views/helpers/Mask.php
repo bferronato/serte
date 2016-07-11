9f83b4ef180a6a2af12207ca177ab0dbd6b5158b
Bruno <bferronato@gmail.com>
2016-07-11 00:54:01 -0300

    Update Mask.php

--- application/views/helpers/Mask.php
+++ application/views/helpers/Mask.php
@@ -10,16 +10,16 @@ class Zend_View_Helper_Mask extends Zend_View_Helper_Abstract
     public function mask($mask, $str)
 	{
 		$mask = $this->getMask($mask);
-
+	
 		if(!$str) $mask = $str;
-
+	
 		$str = str_replace(" ", "", $str);
 		for($i = 0; $i < strlen($str); $i++) {
 			$mask[strpos($mask, "#")] = $str[$i];
 		}
 		return $mask;
 	}
-
+	
 	private function getMask($mask)
 	{
 		switch($mask) {
@@ -36,7 +36,7 @@ class Zend_View_Helper_Mask extends Zend_View_Helper_Abstract
 				$mask = Zend_View_Helper_Mask::CNPJ;
 			break;
 		}
-
+	
 		return $mask;
 	}
 
