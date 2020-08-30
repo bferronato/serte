02cc1712af5e89e93002e172e52456a51e17543c
Bruno Henrique Ferronato <bferronato@gmail.com>
2020-08-30 14:27:34 -0300

    Ajuste: Deprecated: The each() function is deprecated. This message will be suppressed on further calls

--- library/Zend/Cache/Backend.php
+++ library/Zend/Cache/Backend.php
@@ -83,7 +83,9 @@ class Zend_Cache_Backend
     public function setDirectives($directives)
     {
         if (!is_array($directives)) Zend_Cache::throwException('Directives parameter must be an array');
-        while (list($name, $value) = each($directives)) {
+
+        // Ajuste realizado dia 30/08/2020
+        foreach ($directives as $name => $value){
             if (!is_string($name)) {
                 Zend_Cache::throwException("Incorrect option name : $name");
             }
@@ -91,7 +93,6 @@ class Zend_Cache_Backend
             if (array_key_exists($name, $this->_directives)) {
                 $this->_directives[$name] = $value;
             }
-
         }
 
         $this->_loggerSanity();
